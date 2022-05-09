b00310add1829e5a513859173957a736e6d7b352
Michael Chen <michael.chen.1120@gmail.com>
2022-05-09 19:31:01 -0400

    Fix application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
     end
 
     def require_logged_out
-        redirect_to users_url if logged_in?
+        redirect_to cats_url if logged_in?
     end
 
     def require_owned_cat
